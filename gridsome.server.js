e139b39fe52cf20b206f29a1ae13f765c8c281a0
Michael Harper <Mike@microsoftlicensemoneypennyc.onmicrosoft.com>
2020-07-10 09:02:59 +0100

    more api testing

--- gridsome.server.js
+++ gridsome.server.js
@@ -7,15 +7,41 @@
 const axios = require("axios");
 module.exports = function(api) {
   api.loadSource(async (actions) => {
-    const { data } = await axios.get("/api/products");
-    const collection = actions.addCollection("Product");
-    for (const item of data) {
-      collection.addNode({
-        id: item.id,
-        name: item.name,
-        description: item.description,
-        quantity: item.quantity,
-      });
+    try {
+      const { data } = await axios.get("/api/products");
+      // Success 🎉
+      const collection = actions.addCollection("Product");
+      for (const item of data) {
+        collection.addNode({
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          quantity: item.quantity,
+        });
+      }
+      console.log(data);
+    } catch (error) {
+      // Error 😨
+      if (error.response) {
+        /*
+         * The request was made and the server responded with a
+         * status code that falls out of the range of 2xx
+         */
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        /*
+         * The request was made but no response was received, `error.request`
+         * is an instance of XMLHttpRequest in the browser and an instance
+         * of http.ClientRequest in Node.js
+         */
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error);
     }
   });
 
