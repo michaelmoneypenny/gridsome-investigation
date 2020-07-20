a92ce0e80141aa25c50d952c6414c1d6d2f9b4d2
Michael Harper <Mike@microsoftlicensemoneypennyc.onmicrosoft.com>
2020-07-20 13:42:30 +0100

    updates

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,23 +1,32 @@
 <template>
   <Layout>
-    <h1>Form integration</h1>
-    <p>
-      Use a third party serverless form integration + zapier
-    </p>
-    <hr />
-    <form
-      v-on:submit.prevent="onSubmit"
-      action="https://usebasin.com/f/56c24a78093b"
-      method="POST"
-      id="my-contact-form"
-    >
-      <label for="name">Name</label>
-      <input type="text" id="name" name="name" required />
-      <label for="email-address">Email Address</label>
-      <input type="email" id="email" name="email" required />
-      <button type="submit">Submit</button>
-      <div id="form-message"></div>
-    </form>
+    <div>
+      <h1>Form integration</h1>
+      <p>
+        Use a third party serverless form integration + zapier
+      </p>
+      <hr />
+
+      <div class="flex-container">
+        <form
+          class="flex-item"
+          v-on:submit.prevent="onSubmit"
+          action="https://usebasin.com/f/56c24a78093b"
+          method="POST"
+          id="my-contact-form"
+        >
+          <label for="name">Name</label><br />
+          <input type="text" id="name" name="name" required /><br />
+          <label for="email-address">Email Address</label><br />
+          <input type="email" id="email" name="email" required /><br />
+          <input type="text" name="channel" value="online" hidden />
+          <input type="text" name="source" value="test form" hidden />
+          <input type="text" name="family" value="test family" hidden />
+          <button type="submit">Submit</button>
+        </form>
+        <div id="form-message" class="flex-item">{{ formMessage }}</div>
+      </div>
+    </div>
   </Layout>
 </template>
 
@@ -27,6 +36,11 @@ export default {
   metaInfo: {
     title: "About us",
   },
+  data: function() {
+    return {
+      formMessage: "",
+    };
+  },
 
   methods: {
     onSubmit: function() {
@@ -35,7 +49,7 @@ export default {
 
       axios.post(form.action, formData).then(
         (resp) => {
-          console.log(resp);
+          this.formMessage = "Success";
         },
         (resp) => alert("error")
       );
@@ -43,3 +57,12 @@ export default {
   },
 };
 </script>
+<style>
+.flex-container {
+  display: flex;
+  align-content: space-between;
+}
+.flex-item {
+  flex-grow: 1;
+}
+</style>
